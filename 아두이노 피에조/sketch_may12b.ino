3553efc3a2085be192dfe02e3ad15f01e2ae2a84
inhwan <inhwan991120>
2023-05-12 21:15:56 +0900

    아두이노 피에조

---
+++ 아두이노 피에조/sketch_may12b.ino
@@ -0,0 +1,60 @@
+int buzzer = 8;
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 440 ,250);
+  delay(325);
+  tone(buzzer, 440 ,250);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 330 ,550);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 330 ,250);
+  delay(325);
+  tone(buzzer, 330 ,250);
+  delay(325);
+  tone(buzzer, 293 ,750);
+  delay(325);
+  noTone(buzzer);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 440 ,250);
+  delay(325);
+  tone(buzzer, 440 ,250);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 329 ,500);
+  delay(325);
+  tone(buzzer, 392 ,250);
+  delay(325);
+  tone(buzzer, 329 ,250);
+  delay(325);
+  tone(buzzer, 293 ,250);
+  delay(325);
+  tone(buzzer, 329 ,250);
+  delay(325);
+  tone(buzzer, 261 ,750);
+  delay(325);
+  noTone(buzzer);
+  delay(325);
+}
